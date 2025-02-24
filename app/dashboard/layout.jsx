250b136a9503bad84278f83da23c61cbfec4605f
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-02-24 12:08:46 +0530

    Update layout.jsx

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Header from './_components/Header'
 
-function DashboardLayout() {
+function DashboardLayout({children}) {
     return (
-        <div>layout</div>
+        <div>
+            <Header />
+            <div className='mx-5 md:mx-20 lg:mx-36'>
+                {children}
+            </div>
+        </div>
     )
 }
 
